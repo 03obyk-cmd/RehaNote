f13bd101ff9b771b1d3f83b178d8ae8451788453
03obyk-cmd <252697084+03obyk-cmd@users.noreply.github.com>
2026-04-27 05:43:45 +0000

    レイアウト途中まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  before_action :set_communities
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   private
+
+  def set_communities
+    @communities = Community.all
+  end
 
   def after_authentication_url
     mypage_path
